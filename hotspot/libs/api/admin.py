905c4f574d7919b2143cb8faf9b1d0ed9dd81bdd
Nick Sweeting <nikisweeting@gmail.com>
2014-01-13 02:43:52 -0800

    made api folder central

--- hotspot/libs/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-import core.models as api
-
-admin.site.register(api.Hotspot)
-admin.site.register(api.Business)
-admin.site.register(api.CheckIn)
