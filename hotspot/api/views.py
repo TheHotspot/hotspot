7280044d042e0e48ddf244f0864f5d6977203c08
Nick Sweeting <nikisweeting@gmail.com>
2014-01-13 03:45:49 -0800

    first api url routing

--- hotspot/api/views.py
+++ hotspot/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+from models import User
+from models import UserProfile
+from models import Business
+from models import Hotspot
+from models import CheckIn
+
+def docs(request):
+    return HttpResponse("api responds with: api docs")
+
+def hotspot_by_id(request, hotspot_id):
+    return HttpResponse("api responds with: hotspot/%s" % hotspot_id)
+
+def hotspot_by_name(request, hotspot_name):
+    return HttpResponse("api responds with: hotspot/%s" % hotspot_name)
+
+def user(request, username):
+    return HttpResponse("api responds with: user/%s" % username)
--- hotspot/urls.py
+++ hotspot/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from django.contrib import admindocs
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,9 +10,13 @@ urlpatterns = patterns('',
 
     # Admin URLs
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^admin/doc/', include(admindocs.site.urls)),
-
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Other URLs
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^$',          include('hotspot.apps.desktop.urls')),
+    #url(r'^.*.html',    include('hotspot.apps.desktop.urls')),
+
+    url(r'^mobile/',    include('hotspot.apps.mobile.urls')),
+    url(r'^api/',       include('hotspot.api.urls')),
+    url(r'^accounts/',  include('allauth.urls')),
 )
