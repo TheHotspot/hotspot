799eb9447198f4da7ecb86c5f23c20957d96bb8f
Nick Sweeting <nikisweeting@gmail.com>
2014-06-05 16:39:13 -0700

    fixed broken migration

--- hotspot/api/models.py
+++ hotspot/api/models.py
@@ -51,7 +51,7 @@ class User(AbstractUser):
 
     gender = models.CharField(max_length=1, blank=True, default="")
     status = models.IntegerField(blank=True, default=0)
-    birthdate = models.DateField(blank=True, default=None)
+    birthdate = models.DateField(blank=True)
 
     def checkin(self, hotspot, time_in=timezone.now(), time_out=epoch):
         """
